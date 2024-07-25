fa7e7cca37799cf29c39dc8d1ba30c5bb141e687
Parag Sharma <sharmaparag2004@gmail.com>
2024-07-25 13:59:14 +0530

    models update

--- blogwebsite/blogApp/models.py
+++ blogwebsite/blogApp/models.py
@@ -9,3 +9,11 @@ class BlogDetail(models.Model):
   blog_date=models.DateField()
   class Meta:
       db_table = "BlogTable"
+
+class Blogreviews(models.Model):
+   blog_id = models.AutoField(primary_key=True)
+   blog_review_title = models.CharField(max_length=50)
+   blog_review_desc = models.CharField(max_length=250)
+   blog_review_date=models.DateField()
+   class Meta:
+      db_table = "BlogReviewTable"
