d18d8cc36e0c1123f50bb7d168bb15250b900d23
Parag Sharma <sharmaparag2004@gmail.com>
2024-07-25 13:48:40 +0530

    models updated

--- blogwebsite/blogApp/models.py
+++ blogwebsite/blogApp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class BlogDetail(models.Model):
+  blog_id = models.AutoField(primary_key=True)
+  blog_title = models.CharField(max_length=50)
+  blog_desc = models.CharField(max_length=250)
+  blog_date=models.DateField()
+  class Meta:
+      db_table = "BlogTable"
