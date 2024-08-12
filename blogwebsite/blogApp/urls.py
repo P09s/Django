7960dfbfaea27ae29e2c5efcf733596302dd9119
Paramveer032005 <paramveer1433@gmail.com>
2024-08-12 10:34:25 +0530

    backened edited please verify and update. :)

--- blogwebsite/blogApp/urls.py
+++ blogwebsite/blogApp/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path,include
 from .views import indexV
+from . import views
 
 urlpatterns = [
-    path('index/', indexV)
+   path('', indexV, name='home'),
+   path('login_user/', views.login_user, name='login'),
+   path('signup/', views.signup_user, name='signup'),
+   path('home/',views.home, name='home1'),
 ]
+
+
