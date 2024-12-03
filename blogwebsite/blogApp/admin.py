86596a930fd0eeb28301f2f438dc381fce5de08b
Parag Sharma <sharmaparag2004@gmail.com>
2024-12-03 09:48:42 +0530

    changes

--- blogwebsite/blogApp/admin.py
+++ blogwebsite/blogApp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from.models import BlogDetail
+admin.site.site_header = "BloGeeks Admin"
+admin.site.index_title = "Blogs"
+admin.site.site_title = "Blog administration"
+admin.site.register(BlogDetail)
 
 # Register your models here.
